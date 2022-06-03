3bddcb47e2be7124c7ee8231ee003e0d10292e14
Luca Lee <30341309+lucalee-1@users.noreply.github.com>
2022-06-03 19:59:52 +0900

    Exercises 3.7 & 3.8

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -1,7 +1,29 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
+morgan.token("reqData", (req, res) => {
+  return JSON.stringify(req.body);
+});
+
 app.use(express.json());
+app.use(
+  morgan("tiny", {
+    skip: (req, res) => {
+      return req.method === "POST";
+    },
+  })
+);
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :reqData",
+    {
+      skip: (req, res) => {
+        return req.method !== "POST";
+      },
+    }
+  )
+);
 
 let persons = [
   {
